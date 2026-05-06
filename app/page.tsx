4e32e0fb3b2a67a269d9f29c289fa10a3c74d99c
AEGLOBALUSA <ae@futures.global>
2026-05-06 09:59:12 -0400

    feat(home): wire HomeAskMilo + HomeNumbers into page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { HomeHero } from "@/components/home/HomeHero";
 import { HomeMoments } from "@/components/home/HomeMoments";
+import { HomeAskMilo } from "@/components/home/HomeAskMilo";
 import { HomePastors } from "@/components/home/HomePastors";
 import { HomeCampuses } from "@/components/home/HomeCampuses";
 import { HomeFamily } from "@/components/home/HomeFamily";
 import { HomeRhythms } from "@/components/home/HomeRhythms";
+import { HomeNumbers } from "@/components/home/HomeNumbers";
 import { HomeInvitation } from "@/components/home/HomeInvitation";
 import { HomeVoices } from "@/components/home/HomeVoices";
 import { HomeMosaic } from "@/components/home/HomeMosaic";
@@ -17,12 +19,14 @@ export default async function HomePage() {
       <main className="bg-[#FDFBF6] text-[#1C1A17] selection:bg-[#C8906B] selection:text-[#FDFBF6]">
         <HomeHero />
         <HomeMoments />
+        <HomeAskMilo />
         <HomePastors />
         <HomeCampuses />
         <HomeFamily />
         <HomeVoices />
         <HomeMosaic />
         <HomeRhythms />
+        <HomeNumbers />
         <HomeInvitation />
       </main>
     </SlotProvider>
